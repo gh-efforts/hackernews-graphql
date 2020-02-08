ee8a526fd267261c720e224ae83dcba69f19c350
lastingman <keithleaves@gmail.com>
2020-02-08 19:26:01 +0800

    feat: optimize firebase

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,31 @@ firebase.initializeApp(firebaseConfig)
 
 const app = firebase.database().ref(version)
 
-export const findIdsByType = type => {
-  return app
-    .child(`${type}stories`)
-    .once('value')
-    .then(snapshot => {
-      return snapshot.val()
-    })
+export const findIdsByType = (type, first, last, skip) => {
+  if (!skip) {
+    skip = 0
+  }
+  if (first) {
+    return app
+      .child(`${type}stories`)
+      .limitToFirst(first + skip)
+      .once('value')
+      .then(snapshot => snapshot.val())
+      .then(val => val.slice(skip))
+  } else if (last) {
+    return app
+      .child(`${type}stories`)
+      .limitToLast(last + skip)
+      .once('value')
+      .then(snapshot => snapshot.val())
+      .then(val => Object.values(val))
+  } else {
+    return app
+      .child(`${type}stories`)
+      .once('value')
+      .then(snapshot => snapshot.val())
+      .then(val => val.slice(skip))
+  }
 }
 
 export const findItem = id => {
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,20 +1,7 @@
 export const Query = {
   stories: async (_, args, ctx) => {
-    const { type, first, last } = args
-    let { skip } = args
-    let ids = await ctx.db.findIdsByType(type)
-    if (!skip) {
-      skip = 0
-    }
-    if (first) {
-      ids = ids.slice(skip, first)
-    } else if (last) {
-      ids = ids.reverse()
-      ids = ids.slice(skip, last)
-      ids = ids.reverse()
-    } else {
-      ids = ids.slice(skip)
-    }
+    const { type, first, last, skip } = args
+    const ids = await ctx.db.findIdsByType(type, first, last, skip)
     const items = await ctx.db.findItems(ids)
     return items
   },
